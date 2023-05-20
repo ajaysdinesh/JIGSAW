73cd9078a59a1de77e0d20daa85b8501be3a3128
ajaysdinesh <ajaysdinesh@gmail.com>
2023-05-20 12:35:25 +0530

    Updated test.py to slice image

--- test.py
+++ test.py
@@ -1,3 +1,21 @@
 import sys
-image = sys.argv[1]
-print(image)
+import numpy
+import cv2
+from scipy import datasets
+import imageio.v2 as imageio
+import matplotlib.pyplot as plt
+
+image_path = sys.argv[1]
+image = cv2.imread(image_path)
+cv2.namedWindow('custom window', cv2.WINDOW_KEEPRATIO)
+cv2.imshow('custom window', image)
+cv2.resizeWindow('custom window', 1500  , 1000)
+
+
+for r in range(0,image.shape[0],500):
+    for c in range(0,image.shape[1],500):
+        cv2.imwrite(f"img{r}_{c}.png",image[r:r+500, c:c+500,:])
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()        
+
