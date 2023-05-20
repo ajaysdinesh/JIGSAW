df06664b720f8563f299c212cdd54c92e9c518c0
ajaysdinesh <ajaysdinesh@gmail.com>
2023-05-20 12:37:21 +0530

    Slicer

--- test.py
+++ test.py
@@ -11,10 +11,10 @@ cv2.namedWindow('custom window', cv2.WINDOW_KEEPRATIO)
 cv2.imshow('custom window', image)
 cv2.resizeWindow('custom window', 1500  , 1000)
 
-
+pieces = []
 for r in range(0,image.shape[0],500):
     for c in range(0,image.shape[1],500):
-        cv2.imwrite(f"img{r}_{c}.png",image[r:r+500, c:c+500,:])
+        pieces.append(image[r:r+500, c:c+500,:])
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()        
